8730909d39f7876af67e2c7364cc347e518e4567
asamuzak09 <ekazumasa.09300201@gmail.com>
2022-08-05 23:33:44 +0900

    spec修正

--- spec/requests/tasks_controller_spec.rb
+++ spec/requests/tasks_controller_spec.rb
@@ -4,10 +4,19 @@ require 'rails_helper'
 
 describe TasksController, type: :request do
   describe 'PATCH #update' do
-    subject { put '/tasks/update', params: { task: { status: Task::STATUS[:doing] } } }
-    let!(:task) { FactoryBot.create(:task, :todo) }
+    subject { put "/tasks/#{task.id}", params: params }
+    let!(:params) {
+      {
+        title: Faker::Book.title,
+        status: Task::STATUS[:doing],
+        description: Faker::String.random,
+        due_date: Faker::Date.between(from: Date.current - 30.days, to: Date.current + 30.days)
+      }
+    }
 
     context 'タスクがtodoの場合' do
+      let!(:task) { FactoryBot.create(:task, :todo) }
+
       it 'doingにステータスが変更されること' do
         expect { subject }.to change { Task.find(task.id).status }.from(Task::STATUS[:todo]).to(Task::STATUS[:doing])
       end
