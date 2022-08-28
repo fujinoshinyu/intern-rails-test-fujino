9ecb2e68a81bee21a593f4dbc9e0c09ccc87fa38
fujino shinyu <79220429+fujinoshinyu@users.noreply.github.com>
2022-08-29 00:07:53 +0900

    Update task.rb
    動作確認(タイトルを入力してください⇨して下さーーい)

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,7 +5,7 @@ class Task < ApplicationRecord
   STATUS = { todo: 1, doing: 2, review: 3, completed: 4 ,maboroshi: 5 }.freeze
 
   validates :status, inclusion: { in: STATUS.values, message: '存在しないタスクステータスが指定されました' }
-  validates :title, presence: { message: 'タイトルを入力してください' }
+  validates :title, presence: { message: 'タイトルを入力してくださ---い' }
   validates :description, presence: { message: 'タスク詳細を入力してください' }
   validates :due_date, presence: { message: '期限を入力してください' }
   validate :status_check, on: :update
