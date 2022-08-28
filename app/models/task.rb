7855c38234dbe8ebc8351d6ce396e7fbbc615141
fujino shinyu <79220429+fujinoshinyu@users.noreply.github.com>
2022-08-29 00:01:51 +0900

    Update task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@
 
 class Task < ApplicationRecord
   # Railsを知らなくても読解可能なように敢えてenumは使用しません
-  STATUS = { todo: 1, doing: 2, review: 3, completed: 4 }.freeze
+  STATUS = { todo: 1, doing: 2, review: 3, completed: 4 ,maboroshi: 5}.freeze
 
   validates :status, inclusion: { in: STATUS.values, message: '存在しないタスクステータスが指定されました' }
   validates :title, presence: { message: 'タイトルを入力してください' }
@@ -19,6 +19,8 @@ class Task < ApplicationRecord
     logger.debug("変更後のステータスはstatusで取得可能です。\n値：#{status}")
 
     # 更新を中止するため、エラーを格納する
+    
+    
     # errors.add(:status, 'エラーメッセージ')
   end
 end
