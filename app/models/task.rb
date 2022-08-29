bdd497968ad060085c323a2ed640b99527cb0a15
fujinoshinyu <79220429+fujinoshinyu@users.noreply.github.com>
2022-08-30 02:10:25 +0900

    test-kadai

--- app/models/task.rb
+++ app/models/task.rb
@@ -17,17 +17,24 @@ class Task < ApplicationRecord
     # 変更前のステータス
     logger.debug("変更前のステータスはattribute_in_database(:status)で取得可能です。値：#{attribute_in_database(:status)}")
     # 変更後のステータス
-     logger.debug("変更後のステータスはstatusで取得可能です。\n値：#{status}")
+     logger.debug("変更後のステータスはstatusで取得可能です。\n値:#{status}")
+
+
 
     ##個人メモ
     #↓まずは変更前にtodoを取得した場合、次にdoing以外のステータスを取得することができないようにするためのメソッド
+    { in: STATUS.todo: 1 } = attribute_in_database(:status) #←変数をどういった記述で代入すれば良いか分かりませんでした。
+
     #変数名の代入の際の書き方が分かりませんでした。やりたかったこととしては、STATUSのtodoのみの値を変数名に代入し、todoのみを取得する予定でした。
-    { in: STATUS.todo: 1 }= attribute_in_database(:status)
+    #またSTATUS.will_save_change_to_attribute?("status")=>true/falseで指定したカラム（ステータス）に対してtrueかfalseを返し、エラーを発生させることも考えましたが、コーディングが困難でした。
+
+    
 
 
     #↓次にtodoのステータスが取得された場合、doing以外のステータスは更新できないようにするためのメソッドが必要ということは理解できますが、
-    #先ほどと同じく変数の代入とdoing以外でエラー文を表示させるために、「# errors.add(:status, 'エラーメッセージ')」をどう活かせば良いか分かりませんでした。
-    { in: STATUS.todo: 1, STATUS.review: 3, STATUS.completed: 4 } = status(:status)
+    { in: STATUS.todo: 1, STATUS.review: 3, STATUS.completed: 4 } = status(:status) #←変数をどういった記述で代入すれば良いか分かりませんでした
+     #先ほどと同じく変数の代入とdoing以外でエラー文を表示させるために、「# errors.add(:status, 'エラーメッセージ')」をどう活かせば良いか分かりませんでした。
+
 
 
 
