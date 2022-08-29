10effb8b03566912d48899688f049b1016c834ef
fujinoshinyu <79220429+fujinoshinyu@users.noreply.github.com>
2022-08-29 22:28:59 +0900

    give up

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,10 +5,7 @@ class Task < ApplicationRecord
   STATUS = { todo: 1, doing: 2, review: 3, completed: 4 }.freeze
 
 
-    validates :status, inclusion: { in: STATUS.values, message: '存在しないタスクステータスが指定されました'}
-  
-    #↓exclusion（指定した値が含まれていないかを検証する）を用いて、ステータスのdoing以外を入力すればそれ以外のステータスを選んだ際にエラーになるのではと思いましたが、根本的に考え方が間違っていると感じます。
-  validates :status, exclusion: { in: ['todo', 'review', 'completed'], message: "選択できるのはdoingのみです" }
+  validates :status, inclusion: { in: STATUS.values, message: '存在しないタスクステータスが指定されました'}
   validates :title, presence: { message: 'タイトルを入力してください' }
   validates :description, presence: { message: 'タスク詳細を入力してください' }
   validates :due_date, presence: { message: '期限を入力してください' }
@@ -18,14 +15,27 @@ class Task < ApplicationRecord
 
   def status_check
     # 変更前のステータス
-    logger.debug("変更前のステータスはattribute_in_database(:status)で取得可能です。\n値：#{attribute_in_database(:status)}")
+    logger.debug("変更前のステータスはattribute_in_database(:status)で取得可能です。値：#{attribute_in_database(:status)}")
     # 変更後のステータス
-    logger.debug("変更後のステータスはstatusで取得可能です。\n値：#{status}")
+     logger.debug("変更後のステータスはstatusで取得可能です。\n値：#{status}")
 
-    # 更新を中止するため、エラーを格納する
+    ##個人メモ
+    #↓まずは変更前にtodoを取得した場合、次にdoing以外のステータスを取得することができないようにするためのメソッド
+    #変数名の代入の際の書き方が分かりませんでした。やりたかったこととしては、STATUSのtodoのみの値を変数名に代入し、todoのみを取得する予定でした。
+    { in: STATUS.todo: 1 }= attribute_in_database(:status)
+
+
+    #↓次にtodoのステータスが取得された場合、doing以外のステータスは更新できないようにするためのメソッドが必要ということは理解できますが、
+    #先ほどと同じく変数の代入とdoing以外でエラー文を表示させるために、「# errors.add(:status, 'エラーメッセージ')」をどう活かせば良いか分かりませんでした。
+    { in: STATUS.todo: 1, STATUS.review: 3, STATUS.completed: 4 } = status(:status)
+
+
+
+  # 更新を中止するため、エラーを格納する
 
     # errors.add(:status, 'エラーメッセージ')
-    
+    errors.add(:status, 'doingのみしか選択できません。')
+
   end
 end
 
